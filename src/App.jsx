5bfa51ff9f5527589d5928df9dac87f20cca2f1f
Rabeeh <rabirabz001@gmail.com>
2023-06-19 06:27:58 +0530

    firts commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import MuiTypoGraphy from './Components/MuiTypoGraphy'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     < MuiTypoGraphy/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/MuiTypoGraphy.jsx
@@ -0,0 +1,25 @@
+import { Typography } from '@mui/material'
+
+const MuiTypoGraphy = () => {
+  return (
+    <div>
+      <Typography variant='h1'>Hello world</Typography>
+      <Typography variant='h2'>Hello world</Typography>
+      <Typography variant='h3'>Hello world</Typography>
+
+      <Typography variant='subtitle1'>Subtitle</Typography>
+      <Typography variant='subtitle2'>Subtitle</Typography>
+
+      <Typography variant='body1'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Cum voluptatem architecto saepe ratione dolores odio libero tenetur harum non ducimus, molestias
+        nesciunt tempora, distinctio minima iure quidem ipsa? Natus, aspernatur.</Typography>
+      <Typography variant='body2'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+
+        Cum voluptatem architecto saepe ratione dolores odio libero tenetur harum non ducimus, molestias
+        nesciunt tempora, distinctio minima iure quidem ipsa? Natus, aspernatur.</Typography>
+
+    </div>
+  )
+}
+
+export default MuiTypoGraphy
